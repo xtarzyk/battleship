ac70adc3655477274497f8a5669b3b5e56696a92
KamilStarzyk <xtarzyk@tutanota.com>
2020-09-08 20:20:46 +0200

    switch player function

--- battleship.py
+++ battleship.py
@@ -9,3 +9,14 @@ def init_board():
              ['0', '0', '0', '0', '0'],
              ['0', '0', '0', '0', '0']]
     return board
+
+
+
+
+
+def switch_player(player):
+    if player == "Player 1":
+        player = "Player 2"
+    else:
+        player = "Player 1"
+    return player
