5a1c385915071699a03e07384c97e47e9199029d
KamilStarzyk <xtarzyk@tutanota.com>
2020-09-08 22:09:58 +0200

    print board

--- battleship.py
+++ battleship.py
@@ -1,17 +1,34 @@
 
 
 
-
 def init_board():
     board = [['0', '0', '0', '0', '0'],
              ['0', '0', '0', '0', '0'],
              ['0', '0', '0', '0', '0'],
              ['0', '0', '0', '0', '0'],
              ['0', '0', '0', '0', '0']]
+    
     return board
 
+board = init_board()
 
 
+def print_board(board):
+    alpha = ["A","B","C","D","E"]
+    line = '    ---+---+---+---+---'
+    print("     1   2   3   4   5")
+    print("                                     ")
+    counter = 0
+    n = 0
+    for row in board:
+        row_num1, row_num2, row_num3, row_num4, row_num5 = row
+        print(f' {alpha[n]}   {row_num1} | {row_num2} | {row_num3} | {row_num4} | {row_num5}')
+        n += 1
+        if counter < 4:
+            print(line)
+            counter += 1
+
+print_board(board)
 
 
 def switch_player(player):
