47b15ccbc55062181b4f4daf42f2fbb8922541e6
KamilStarzyk <xtarzyk@tutanota.com>
2020-09-09 20:18:36 +0200

    added main, move and mark functions

--- battleship.py
+++ battleship.py
@@ -1,4 +1,12 @@
-
+def main():
+    # tutaj docelowo będą wywoływane wszystkie (albo prawie) funkcje odpowiedzialne
+    # za działanie gry
+    ship = "X"
+    board = init_board()
+    print_board(board)
+    moove = move(board)
+    mark(board, moove[0], moove[1], ship)
+    print_board(board)
 
 
 def init_board():
@@ -10,7 +18,7 @@ def init_board():
     
     return board
 
-board = init_board()
+
 
 
 def print_board(board):
@@ -28,7 +36,65 @@ def print_board(board):
             print(line)
             counter += 1
 
-print_board(board)
+
+
+
+
+
+def move(board):
+    x = True
+    while x:
+        position = input("Enter coordinates for your ship: ").upper()
+
+        if "1" in position:
+            col = 0
+        elif "2" in position:
+            col = 1
+        elif "3" in position:
+            col = 2
+        elif "4" in position:
+            col = 3
+        elif "5" in position:
+            col = 4
+        else:
+            col = "not valid"
+
+        if "A" in position:
+            row = 0
+        elif "B" in position:
+            row = 1
+        elif "C" in position:
+            row = 2
+        elif "D" in position:
+            row = 3
+        elif "E" in position:
+            row = 4
+        else:
+            row = "not valid"
+
+        if len(position) > 2:
+            row = "not valid"
+        
+        if row or col == "not valid":
+            print_board(board)
+            print("\n Enter a valid coordinates!")
+        elif board[col][row] != "0":
+            print_board(board)
+            print("\nYou have a ship here, already.")
+        else:
+            x = False
+
+    return col, row
+
+
+def mark(board, row, col, ship):
+    board[row][col] = ship
+    return board
+
+
+
+
+
 
 
 def switch_player(player):
@@ -37,3 +103,7 @@ def switch_player(player):
     else:
         player = "Player 1"
     return player
+
+
+
+main()
